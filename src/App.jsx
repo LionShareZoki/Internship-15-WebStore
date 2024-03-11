7d74bc6eb58cd927d3c314a293a719fe47d83592
LionShareZoki <pujic.zoran@gmail.com>
2024-03-11 18:57:49 +0100

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage/ProductsPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import Header from "./components/Header/Header";
+
 function App() {
   return (
     <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductsPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:productId" element={<ProductPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
